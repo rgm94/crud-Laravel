b6fb61e9ce2dc37d74eda259d48fc8c20288cac8
rgm94 <rgmartos1994@gmail.com>
2025-10-03 08:22:24 +0200

    Correción del nombre del método de la ruta POST

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 
 Route::get('/index',[CrudController::class,'showAll'])->name('index');
 Route::get('/create-user',[CrudController::class,'createUser'])->name('create');
-Route::post('/new-user',[CrudController::class,'saveNewUser'])->name('save');
+Route::post('/new-user',[CrudController::class,'saveUser'])->name('save');
 Route::get('/edit-user/{id}',[CrudController::class,'editUser'])->name('edit');
 Route::put('/update-user/{id}',[CrudController::class,'updateUser'])->name('update');
 Route::delete('/delete-user/{id}',[CrudController::class,'deleteUser'])->name('delete');
